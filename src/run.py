64e4100511e2779920d402d6dde64efeb57c151d
tomhea <tomhe@campus.technion.ac.il>
2021-03-04 02:46:46 +0200

    newline, expr-fj fixes
    files can end without NL
    
    FJ ops only-flip without ; like:
      IO
      x+47
    now get the line number of the op from the expr (instead of fishy ways.

--- src/run.py
+++ src/run.py
@@ -88,8 +88,8 @@ def assemble_and_run(input_files, preprocessed_file=None, output_file=None, defi
 def main():
     for test, _input in (('cat', "Hello World!\0"), ('ncat', ''.join(chr(255-ord(c)) for c in 'Flip Jump Rocks!\0')),
                          ('testbit', ''), ('testbit_with_nops', ''), ('mathbit', ''), ('mathvec', ''), ('not', ''),
-                         ('rep', ''), ('ncmp', ''), ('nadd', ''), ('hexprint', '')):
-        # if test != 'hexprint':
+                         ('rep', ''), ('ncmp', ''), ('nadd', ''), ('hexprint', ''), ('simple', '')):
+        # if test != 'simple':
         #     continue
         print(f'running test {test}({_input}):')
         assemble_and_run([f'tests/{test}.fj'], preprocessed_file=f'tests/compiled/{test}__no_macros.fj',
