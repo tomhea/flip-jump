ef7765b6c3377a393f6eb521664e446b69ada5af
Tomhe <tomherman4@gmail.com>
2022-06-03 14:26:01 +0300

    moved configs from tests to conftest

--- src/conftest.py
+++ src/conftest.py
@@ -1,8 +1,50 @@
-from src.tests import get_compile_tests_args_from_csv, get_run_tests_args_from_csv
-from src.tests import CompileCSVs, RunCSVs
+import csv
+from typing import List
+from pathlib import Path
+
+# Don't remove the "src." from src.tests. It will break the Assertion-info on failed assertions in tests.
+from src.tests import CompileTestArgs, RunTestArgs
+
+
+CompileCSVs = {
+    'fast': 'tests/test_compile_fast.csv',
+    'medium': 'tests/test_compile_medium.csv',
+    'slow': 'tests/test_compile_slow.csv',
+    'hexlib': 'tests/test_compile_hexlib.csv'
+}
+
+
+RunCSVs = {
+    'fast': 'tests/test_run_fast.csv',
+    'medium': 'tests/test_run_medium.csv',
+    'slow': 'tests/test_run_slow.csv',
+    'hexlib': 'tests/test_run_hexlib.csv'
+}
 
 
 TEST_TYPES = ('fast', 'medium', 'slow', 'hexlib')
+
+
+def argument_line_iterator(csv_file_path: str, num_of_args: int):
+    with open(csv_file_path, 'r') as csv_file:
+        for line_index, line in enumerate(csv.reader(csv_file)):
+            if line:
+                assert len(line) == num_of_args, f'expects {num_of_args} args, got {len(line)} ' \
+                                                 f'(file {Path(csv_file_path).absolute()}, line {line_index + 1})'
+                yield map(str.strip, line)
+
+
+# TODO maybe use pytest-dependency in the future,
+#  to assure that if both a run and compile are tested - the run won't run before the compile,
+#  and wil be skipped if compilation failed.
+
+
+def get_compile_tests_args_from_csv(csv_file_path: str) -> List[CompileTestArgs]:
+    return [CompileTestArgs(*line) for line in argument_line_iterator(csv_file_path, CompileTestArgs.num_of_args)]
+
+
+def get_run_tests_args_from_csv(csv_file_path: str) -> List[RunTestArgs]:
+    return [RunTestArgs(*line) for line in argument_line_iterator(csv_file_path, RunTestArgs.num_of_args)]
 
 
 def pytest_addoption(parser):
@@ -18,12 +60,14 @@ def pytest_generate_tests(metafunc):
     if "compile_args" in metafunc.fixturenames:
         for test_type in TEST_TYPES:
             if all_tests or metafunc.config.getoption(test_type):
-                compile_tests.extend(get_compile_tests_args_from_csv(CompileCSVs[test_type]))
+                if test_type in CompileCSVs:
+                    compile_tests.extend(get_compile_tests_args_from_csv(CompileCSVs[test_type]))
         metafunc.parametrize("compile_args", compile_tests, ids=repr)
 
     run_tests = []
     if "run_args" in metafunc.fixturenames:
         for test_type in TEST_TYPES:
             if all_tests or metafunc.config.getoption(test_type):
-                run_tests.extend(get_run_tests_args_from_csv(RunCSVs[test_type]))
+                if test_type in RunCSVs:
+                    run_tests.extend(get_run_tests_args_from_csv(RunCSVs[test_type]))
         metafunc.parametrize("run_args", run_tests, ids=repr)
--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,4 @@
-import csv
 from pathlib import Path
-from typing import List
 
 import assembler
 import defs
@@ -10,18 +8,6 @@ import fjm_run
 CSV_TRUE = 'True'
 CSV_FALSE = 'False'
 CSV_BOOLEAN = (CSV_TRUE, CSV_FALSE)
-
-
-CompileCSVs = {'fast': 'tests/test_compile_fast.csv',
-               'medium': 'tests/test_compile_medium.csv',
-               'slow': 'tests/test_compile_slow.csv',
-               'hexlib': 'tests/test_compile_hexlib.csv'}
-
-
-RunCSVs = {'fast': 'tests/test_run_fast.csv',
-           'medium': 'tests/test_run_medium.csv',
-           'slow': 'tests/test_run_slow.csv',
-           'hexlib': 'tests/test_run_hexlib.csv'}
 
 
 class CompileTestArgs:
@@ -115,25 +101,3 @@ def test_run(run_args: RunTestArgs) -> None:
     output = output.decode('raw_unicode_escape')
     expected_output = run_args.get_expected_output()
     assert output == expected_output
-
-
-def argument_line_iterator(csv_file_path: str, num_of_args: int):
-    with open(csv_file_path, 'r') as csv_file:
-        for line_index, line in enumerate(csv.reader(csv_file)):
-            if line:
-                assert len(line) == num_of_args, f'expects {num_of_args} args, got {len(line)} ' \
-                                                 f'(file {Path(csv_file_path).absolute()}, line {line_index + 1})'
-                yield map(str.strip, line)
-
-
-# TODO maybe use pytest-dependency in the future,
-#  to assure that if both a run and compile are tested - the run won't run before the compile,
-#  and wil be skipped if compilation failed.
-
-
-def get_compile_tests_args_from_csv(csv_file_path: str) -> List[CompileTestArgs]:
-    return [CompileTestArgs(*line) for line in argument_line_iterator(csv_file_path, CompileTestArgs.num_of_args)]
-
-
-def get_run_tests_args_from_csv(csv_file_path: str) -> List[RunTestArgs]:
-    return [RunTestArgs(*line) for line in argument_line_iterator(csv_file_path, RunTestArgs.num_of_args)]
