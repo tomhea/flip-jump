54f62c9c2a1df433caa21ba4497cc3d520965ecd
Tomhe <tomherman4@gmail.com>
2022-06-25 11:23:34 +0300

    add abstract device

---
+++ src/io_devices/IODevice.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class IODevice(ABC):
+    @abstractmethod
+    def read_bit(self) -> bool:
+        return False
+
+    @abstractmethod
+    def write_bit(self, bit: bool) -> None:
+        pass
+
+    @abstractmethod
+    def is_available_read(self) -> bool:
+        return False
+
+    @abstractmethod
+    def is_available_write(self) -> bool:
+        return False
+
+    @abstractmethod
+    def is_eof(self) -> bool:
+        return False
+
+    # Also, each class should implement a "__del__" to flush last changes before it gets deleted.
